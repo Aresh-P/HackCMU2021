af915f3f083a77b01e51a44a64444d663516f244
Luca Borletti <lucaborletti@lucas-mbp.wifi.local.cmu.edu>
2021-10-02 17:29:10 -0400

    a

--- new_ics_parser.py
+++ new_ics_parser.py
@@ -1,57 +1,11 @@
-# from prompt import createStudentGroup
-# from student_info import StudentInfo
+from prompt import createStudentGroup
+from student_info import StudentInfo
 import random
 import os, sys
 import icalendar
 from icalendar import Calendar, Event
 import pytz
 from datetime import datetime, timedelta
-
-class StudentInfo:
-    def __init__(self, blocks, goals, group):
-        self.blocks = blocks
-        self.goals = goals
-        self.group = group
-
-def maxInput(studentName):
-    max = input(f'{studentName}, What is the maximum people you want to work with? --> ')
-    if max.isnumeric():
-        max = int(max)
-    valid = False
-    if isinstance(max,int)==False:
-        while valid == False:
-            max = input(f'Please input a valid number of max people {studentName} --> ')
-            if max.isnumeric():
-                max = int(max)
-            if max < 0:
-                continue
-            else:
-                valid = True
-    return max
-
-def minInput(studentName):
-    min = input(f'{studentName}, What is the minimum people you want to work with? --> ')
-    if min.isnumeric():
-        min = int(min)
-    valid = False
-    if isinstance(min,int)==False:
-        while valid == False:
-            min = input(f'Please input a valid number of max people {studentName} --> ')
-            if min.isnumeric():
-                min = int(min)
-            if min < 0:
-                continue
-            else:
-                valid = True
-    return min
-
-def createStudentGroup(studentName):
-    studentInput = {}
-    max = maxInput(studentName)
-    min = minInput(studentName)
-    studentInput["mingroup"] = min
-    studentInput["maxgroup"] = max
-    return studentInput
 
 def findSundayBaseTime(firstStartTime):
     ####find the sunday before the week specified in the calendar
