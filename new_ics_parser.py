825a1efe45ac8b6db1fc0be4493d7f0067b1f76a
Luca Borletti <lucaborletti@lucas-mbp.wifi.local.cmu.edu>
2021-10-02 17:42:32 -0400

    a

--- new_ics_parser.py
+++ new_ics_parser.py
@@ -1,11 +1,57 @@
-from prompt import createStudentGroup
-from student_info import StudentInfo
+# from prompt import createStudentGroup
+# from student_info import StudentInfo
 import random
 import os, sys
 import icalendar
 from icalendar import Calendar, Event
 import pytz
 from datetime import datetime, timedelta
+
+class StudentInfo:
+    def __init__(self, blocks, goals, group):
+        self.blocks = blocks
+        self.goals = goals
+        self.group = group
+
+def maxInput(studentName):
+    max = input(f'{studentName}, What is the maximum people you want to work with? --> ')
+    if max.isnumeric():
+        max = int(max)
+    valid = False
+    if isinstance(max,int)==False:
+        while valid == False:
+            max = input(f'Please input a valid number of max people {studentName} --> ')
+            if max.isnumeric():
+                max = int(max)
+            if max < 0:
+                continue
+            else:
+                valid = True
+    return max
+
+def minInput(studentName):
+    min = input(f'{studentName}, What is the minimum people you want to work with? --> ')
+    if min.isnumeric():
+        min = int(min)
+    valid = False
+    if isinstance(min,int)==False:
+        while valid == False:
+            min = input(f'Please input a valid number of max people {studentName} --> ')
+            if min.isnumeric():
+                min = int(min)
+            if min < 0:
+                continue
+            else:
+                valid = True
+    return min
+
+def createStudentGroup(studentName):
+    studentInput = {}
+    max = maxInput(studentName)
+    min = minInput(studentName)
+    studentInput["mingroup"] = min
+    studentInput["maxgroup"] = max
+    return studentInput
 
 def findSundayBaseTime(firstStartTime):
     ####find the sunday before the week specified in the calendar
@@ -115,18 +161,16 @@ def createStudentBlock(path):
         return blocks
 
 
-def fromSio(icsFile):
+def fromSio(icsCal):
     ####check if SIO file or not by analyzing traces of SIO formatting
-    icsCal = Calendar.from_ical(icsFile.read())
 
     if "Instructor" in str(icsCal.walk("VEVENT")[0]):
         return True
     
     return False
 
-def createStudentGoals(icsFile):
+def createStudentGoals(icsCal):
     ####generate student goals dictionary where course nums are keys to "goals"
-    icsCal = Calendar.from_ical(icsFile.read())
     
     goals = {}
 
@@ -160,13 +204,16 @@ def createStudentInfos(path):
             
             icsFile = open(f"{path}/{name}/{icsPath}", "rb")
 
-            if fromSio(icsFile):
+            icsCal = Calendar.from_ical(icsFile.read())
 
-                goals = createStudentGoals(icsFile)
 
-        group = createStudentGroup()
+            if fromSio(icsCal):
+
+                goals = createStudentGoals(icsCal)
+
+        group = createStudentGroup(name)
         
-        studentInfos[name] = studentInfo(
+        studentInfos[name] = StudentInfo(
             block,
             goals,
             group
@@ -180,7 +227,9 @@ def createStudentInfos(path):
 print(createStudentInfos("/Users/lucaborletti/Desktop/Hack@CMU/names"))
 
 
-    
+# print(open("/Users/lucaborletti/Desktop/Hack@CMU/names/luca/F21_schedule.ics", "rb"))
+
+# print(os.listdir("/Users/lucaborletti/Desktop/Hack@CMU/names/luca"))
         
 
 
