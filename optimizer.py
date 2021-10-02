a1f54880dcfc8796b3b17a9dfb944f8fa52123e0
Bushwallyta271828 <bushwallyta@gmail.com>
2021-10-02 00:48:21 -0400

    Add files via upload

--- optimizer.py
+++ optimizer.py
@@ -1,18 +1,29 @@
+def cost(user_requirements, proposed_solution):
+    """
+    user_requirements is of the form specified in optimizer.
+    
+    proposed_solution 
+    """
+
+
 def optimizer(user_requirements):
     """
     Input:
     Each user requirement consists of:
-      1.) A list of intervals of times the user is free
-          [(time1, time2), (time3, time4), ...]
+      1.) A sorted list of intervals of times the user is free
+          [{"start": time1, "stop": time2}, ...]
           times are measured in hours from the current time,
           so tomorrow is 24, two days from now is 48, etcetera.
       2.) A list of deadlines and study requirements for each deadline
-          [(time1, course1, hours1), (time2, course2, hours2), ...]
+          [{"time": time1, "course": course1, "hours": hours1}, ...]
       3.) A range of acceptable study group sizes
-          (min study group size, max study group size)
+          {"mingroup": min study group size, "maxgroup": max study group size}
     
     Output:
-    For each study group meeting:
-      1.) [time1, time2, course, [student1, student2, ...]]
+    A list for each study group meeting:
+      1.) {"start": time1,
+           "stop": time2,
+           "course": course,
+           "students": {student1, student2, ...}}
     """
-
+    
